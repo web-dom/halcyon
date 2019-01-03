ee3165dbcfb57c6ec8f95d0567acd1a0f93f23d4
Richard <Richard>
2019-01-03 06:11:45 +0000

    publishing macro implementation

--- halcyon_macro_impl/src/lib.rs
+++ halcyon_macro_impl/src/lib.rs
@@ -2,8 +2,6 @@ extern crate proc_macro;
 
 use proc_macro::{Literal, TokenStream, TokenTree};
 use proc_macro_hack::proc_macro_hack;
-use quote::quote;
-use syn::{parse_macro_input, Expr};
 
 /// Add one to an expression.
 #[proc_macro_hack]
